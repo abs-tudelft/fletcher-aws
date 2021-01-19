5e3edce18bcb54ffb306994941ed8d7477ea8d43
Joost Hoozemans <j.j.hoozemans@tudelft.nl>
2021-01-19 17:03:48 +0100

    Removed 1DDR version.
    The 4DDR version now by default operates with 1 DDR controller, but the others can be enabled by removing the defines in cl_fletcher_aws.sv

--- examples/project-generate.sh
+++ examples/project-generate.sh
@@ -26,29 +26,25 @@ cd "$PROJ_NAME"
 
 
 
-for version in 1DDR 4DDR; do
-  cd $version
-  # copy vhdl dir into project/design
-  cp --recursive --no-dereference "$VHDL_PATH" design/
+# copy vhdl dir into project/design
+cp --recursive --no-dereference "$VHDL_PATH" design/
 
-  # Get an list (in compile order) of source files from vhdeps, place it in build/scripts/encrypt.tcl (replacing #FLETCHER_AXITOP_VHDL_FILES) 
-  # and verif/scripts/top.vivado.vhdl.f
-  if ! cd design/vhdl; then
-    echo "vhdl directory not found, please make sure you have referenced a Fletcher-generated directory that is named 'vhdl'."
-    exit -1
-  else
-    python3 -m vhdeps -i . ${VHDEPS_INCLUDE_ARGS} dump AxiTop > sources.tmp.txt
-    cut -d ' ' -f4 sources.tmp.txt > sources.txt
-    cp sources.txt ../../verif/scripts/top.vivado.vhdl.f #These are now absolute paths
-    
-    sed -e 's<^<file copy -force <' sources.txt > sources.encrypt.tmp.txt
-    sed -e 's<$< $TARGET_DIR<' sources.encrypt.tmp.txt > sources.encrypt.txt
-    sed -e 's<#FLETCHER_AXITOP_VHDL_FILES<cat sources.encrypt.txt; echo "&"<e' ../../build/scripts/encrypt.tcl.template > ../../build/scripts/encrypt.tcl
-    rm ../../build/scripts/encrypt.tcl.template
-    sed -i -e 's<#FLETCHER_AXITOP_VHDL_FILES<<' ../../build/scripts/encrypt.tcl
-    cd ../..
-  fi
-  cd ..
-done;
+# Get an list (in compile order) of source files from vhdeps, place it in build/scripts/encrypt.tcl (replacing #FLETCHER_AXITOP_VHDL_FILES) 
+# and verif/scripts/top.vivado.vhdl.f
+if ! cd design/vhdl; then
+  echo "vhdl directory not found, please make sure you have referenced a Fletcher-generated directory that is named 'vhdl'."
+  exit -1
+else
+  python3 -m vhdeps -i . ${VHDEPS_INCLUDE_ARGS} dump AxiTop > sources.tmp.txt
+  cut -d ' ' -f4 sources.tmp.txt > sources.txt
+  cp sources.txt ../../verif/scripts/top.vivado.vhdl.f #These are now absolute paths
+  
+  sed -e 's<^<file copy -force <' sources.txt > sources.encrypt.tmp.txt
+  sed -e 's<$< $TARGET_DIR<' sources.encrypt.tmp.txt > sources.encrypt.txt
+  sed -e 's<#FLETCHER_AXITOP_VHDL_FILES<cat sources.encrypt.txt; echo "&"<e' ../../build/scripts/encrypt.tcl.template > ../../build/scripts/encrypt.tcl
+  rm ../../build/scripts/encrypt.tcl.template
+  sed -i -e 's<#FLETCHER_AXITOP_VHDL_FILES<<' ../../build/scripts/encrypt.tcl
+  cd ../..
+fi
 
 
